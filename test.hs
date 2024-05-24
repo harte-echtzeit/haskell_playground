425ef73e3981cb27e8e045492868d7309b7c4069
harte_echtzeit <info@harte-echtzeit.com>
2024-05-24 16:25:53 +0200

    some pattern and ca fun 2024-05-24

--- test.hs
+++ test.hs
@@ -13,8 +13,43 @@ recurPrint x = do
 
 reOrgList n x = drop n x ++ take n x
 
-maskPat n _ _ = pure()
-        n l x = do
-  take n (reOrgList n x)
-  maskPat (n-1) l x
+maskPat 0 l x = print (take l x)
+maskPat n l x = do
+  print(take l x)
+  maskPat (n-1) l (reOrgList l x)
+-- here an if clause has to be inserted to catch the case that l>len(x)
 
+-- maybe try to refactor here and return a tuple from fun?
+--maskPat' 0 l x = (take l x, x)
+--maskPat' n l (x, y) = (take l x, (maskPat' (n-1) l (reOrgList l x), 0))
+
+
+-- simple cellular automata
+
+-- later: fix pattern length due to spaces here ->5 means 3 chars 
+nextCA x = take 3 x
+
+-- pattern matching for one CA rule -> can it be automated?
+
+caPat "000" = "1"
+caPat "001" = "0"
+caPat "010" = "1"
+caPat "100" = "0"
+caPat "011" = "1"
+caPat "110" = "0"
+caPat "101" = "1"
+caPat "111" = "0"
+
+caAround x = (caPat (nextCA x))
+
+--iterCA 0 x = return[x]
+--iterCA n x = do
+  --caPat x ++ iterCA (n-1) x
+
+
+something (x,y) = (take 3 x, drop 3 x ++ take 3 x)
+
+someIter 0 (x,y) = return(([], []))
+someIter n (x,y) = do
+  something(x, y)
+  someIter (n-1) (snd (something (x,y)), y)
